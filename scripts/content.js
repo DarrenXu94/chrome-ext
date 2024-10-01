3616acedee84fedd7e4e8953e8ef5c9a98f0708a
Darren Xu <darrenxu1994@gmail.com>
2024-10-01 20:02:55 +1000

    Initial commit

---
+++ scripts/content.js
@@ -0,0 +1,84 @@
+const spoilerChannels = ["Serie A", "NFL", "LALIGA"];
+
+const scoreRegex = /\d{1,2}\s*-\s*\d{1,2}/; // Any string that is a score eg 0 - 1
+
+// Handle in youtube search mode
+const handleVideoRenderer = (videoRenderer) => {
+  const channelName =
+    videoRenderer.querySelector("ytd-channel-name").textContent;
+
+  // if channel name includes any string from spoilerChannels array hide the thumbnail
+  if (spoilerChannels.some((channel) => channelName.includes(channel))) {
+    const coreImage = videoRenderer.querySelector("img.yt-core-image");
+    coreImage.style.display = "none";
+  }
+
+  // check if the title contains a score
+  const title = videoRenderer.querySelector("#video-title-link").textContent;
+
+  if (scoreRegex.test(title)) {
+    const titleElement = videoRenderer.querySelector("#video-title-link");
+    const newTitle = titleElement.textContent.replace(scoreRegex, "X - X");
+    titleElement.textContent = newTitle;
+  }
+};
+
+// Handle in youtube home page mode
+const handleRichGridMedia = (gridMedia) => {
+  const channelName = gridMedia.querySelector(".ytd-channel-name").textContent;
+
+  // if channel name includes any string from spoilerChannels array hide the thumbnail
+  if (spoilerChannels.some((channel) => channelName.includes(channel))) {
+    const coreImage = gridMedia.querySelector("img.yt-core-image");
+    coreImage.style.display = "none";
+
+    // check if the title contains a score
+  }
+  const title = gridMedia.querySelector("#video-title-link").textContent;
+  if (scoreRegex.test(title)) {
+    const titleElement = gridMedia.querySelector("#video-title-link");
+    const newTitle = titleElement.textContent.replace(scoreRegex, "X - X");
+    titleElement.textContent = newTitle;
+  }
+};
+
+function run() {
+  console.log("RUNNING CONTENT SCRIPT");
+
+  const thumbnails = document.querySelectorAll("ytd-thumbnail");
+
+  for (let i = 0; i < thumbnails.length; i++) {
+    // find closest ytd-rich-grid-media or ytd-video-renderer
+
+    const gridMedia = thumbnails[i].closest("ytd-rich-grid-media");
+
+    if (gridMedia) {
+      handleRichGridMedia(gridMedia);
+    }
+
+    const videoRenderer = thumbnails[i].closest("ytd-video-renderer");
+
+    if (videoRenderer) {
+      handleVideoRenderer(videoRenderer);
+    }
+  }
+}
+
+window.onload = run;
+window.addEventListener("yt-navigate-start", run, true);
+
+const targetNode = document.querySelector("#contents"); // Element containing the suggested videos
+const config = { childList: true, subtree: true };
+
+const callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      // console.log("New video suggestions detected!");
+      // Add your logic to handle new suggestions here
+      run();
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
